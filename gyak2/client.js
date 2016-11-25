231e5834209d6cf743ec2aa5851a37b75a9120bb
Balint Soos <soosbalint95@gmail.com>
2016-11-25 21:38:30 +0100

    gyak 2

---
+++ gyak2/client.js
@@ -0,0 +1,21 @@
+const net = require('net')
+const log = require('../logger')
+
+const HOST = '127.0.0.1'
+const PORT = 6969
+
+const client = new net.Socket()
+
+client.connect(PORT, HOST, () => {
+  log.connected('CONNECTED', `${HOST}:${PORT}`)
+})
+
+client.on('data', data => {
+  log.data('DATA', data)
+})
+
+client.on('close', () => {
+  log.closed('CLOSED')
+})
+
+client.write('5 + 2')
---
+++ gyak2/server.js
@@ -0,0 +1,32 @@
+const net = require('net')
+const log = require('../logger')
+
+const host = '127.0.0.1'
+const port = 6969
+
+const server = net.createServer(socket => {
+  log.connected('CONNECTED', getSocketAddress(socket))
+
+  socket.on('data', data => {
+    log.data('DATA', `${getSocketAddress(socket)} ${data}`)
+
+    socket.end(`${new Function('return ' + data)()}`)
+  })
+
+  socket.on('close', data => {
+    log.closed('CLOSED', getSocketAddress(socket))
+  })
+})
+
+server.listen({ host, port }, () => {
+  log.info('Server listening', getServerAddress(server))
+})
+
+function getSocketAddress(socket) {
+  return `${socket.remoteAddress}:${socket.remotePort}`
+}
+
+function getServerAddress(server) {
+  const address = server.address()
+  return `${address.address}:${address.port} (${address.family})`
+}
